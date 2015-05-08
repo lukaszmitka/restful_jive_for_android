e1fa88e9083872641caab1e992cc93389ceffd07
Łukasz Mitka <luk.mitka@uj.edu.pl>
2015-05-08 22:59:13 +0200

    Added default exception bound to server at local network at Solaris.

--- app/src/main/java/pl/edu/uj/synchrotron/restfuljive/CertificateExceptionActivity.java
+++ app/src/main/java/pl/edu/uj/synchrotron/restfuljive/CertificateExceptionActivity.java
@@ -19,6 +19,7 @@ import com.lamerman.SelectionMode;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -47,6 +48,8 @@ public abstract class CertificateExceptionActivity extends WifiMonitorActivity {
 		String lastCertPath = settings.getString("LastCertificatePath", "");
 		if (!lastCertPath.equals("")) {
 			setSSLCertificate(lastCertPath);
+		} else {
+			setSSLCertificate(R.raw.server);
 		}
 	}
 
@@ -137,6 +140,46 @@ public abstract class CertificateExceptionActivity extends WifiMonitorActivity {
 			sslContext = SSLContext.getInstance("TLS");
 			sslContext.init(null, tmf.getTrustManagers(), null);
 			certificateFileInput.close();
+			restartQueue();
+			return true;
+		} catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException | KeyManagementException
+				e) {
+			e.printStackTrace();
+
+		}
+		return false;
+	}
+
+	private boolean setSSLCertificate(int resID) {
+
+		TrustManagerFactory tmf;
+		CertificateFactory cf;
+		Log.d("setSSLCertificate", "Created file");
+		try {
+			/*
+			get certificate file from path specified by user
+			 */
+			InputStream is = getResources().openRawResource(resID);
+
+			//FileInputStream certificateFileInput =  new FileInputStream();
+			cf = CertificateFactory.getInstance("X.509");
+			Certificate ca;
+			ca = cf.generateCertificate(is);
+
+			// Create a KeyStore containing our trusted CAs
+			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			keyStore.load(null, null);
+			keyStore.setCertificateEntry("ca", ca);
+			String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+			// Create a TrustManager that trusts the CAs in our KeyStore
+			tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+			tmf.init(keyStore);
+			// Create an SSLContext that uses our TrustManager
+			sslContext = null;
+			sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+			//certificateFileInput.close();
+			is.close();
 			restartQueue();
 			return true;
 		} catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException | KeyManagementException
--- app/src/main/java/pl/edu/uj/synchrotron/restfuljive/CertificateExceptionFragment.java
+++ app/src/main/java/pl/edu/uj/synchrotron/restfuljive/CertificateExceptionFragment.java
@@ -20,6 +20,7 @@ import com.android.volley.toolbox.Volley;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -45,11 +46,14 @@ public class CertificateExceptionFragment extends Fragment {
 		View view = super.onCreateView(inflater, container, savedInstanceState);
 		SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, FragmentActivity.MODE_PRIVATE);
 		String lastCertPath = settings.getString("LastCertificatePath", "");
-		if (lastCertPath != "") {
+		if (!lastCertPath.equals("")) {
 			Log.d("onCreateView()", "Using certificate path: " + lastCertPath);
 			setSSLCertificate(lastCertPath);
 		} else {
 			Log.d("onCreateView()", "No path for certificate");
+
+			setSSLCertificate(R.raw.server);
+
 		}
 		return view;
 	}
@@ -69,8 +73,8 @@ public class CertificateExceptionFragment extends Fragment {
 			queue.getCache().clear();
 		}
 		if (sslContext != null) {
-			queue = Volley.newRequestQueue(getActivity().getApplicationContext(), new HurlStack(null,
-					sslContext.getSocketFactory()));
+			queue = Volley.newRequestQueue(getActivity().getApplicationContext(),
+					new HurlStack(null, sslContext.getSocketFactory()));
 		} else {
 			queue = Volley.newRequestQueue(getActivity().getApplicationContext());
 		}
@@ -107,6 +111,46 @@ public class CertificateExceptionFragment extends Fragment {
 			certificateFileInput.close();
 			restartQueue();
 
+			return true;
+		} catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException | KeyManagementException
+				e) {
+			e.printStackTrace();
+
+		}
+		return false;
+	}
+
+	private boolean setSSLCertificate(int resID) {
+
+		TrustManagerFactory tmf;
+		CertificateFactory cf;
+		Log.d("setSSLCertificate", "Created file");
+		try {
+			/*
+			get certificate file from path specified by user
+			 */
+			InputStream is = getResources().openRawResource(resID);
+
+			//FileInputStream certificateFileInput =  new FileInputStream();
+			cf = CertificateFactory.getInstance("X.509");
+			Certificate ca;
+			ca = cf.generateCertificate(is);
+
+			// Create a KeyStore containing our trusted CAs
+			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			keyStore.load(null, null);
+			keyStore.setCertificateEntry("ca", ca);
+			String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+			// Create a TrustManager that trusts the CAs in our KeyStore
+			tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+			tmf.init(keyStore);
+			// Create an SSLContext that uses our TrustManager
+			sslContext = null;
+			sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+			//certificateFileInput.close();
+			is.close();
+			restartQueue();
 			return true;
 		} catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException | KeyManagementException
 				e) {
